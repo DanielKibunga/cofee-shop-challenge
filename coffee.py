b7377004a68e78d42f3368a69047685a6158a4eb
Daniel Kimani <daniel.kibunga@students.moringaschool.com>
2025-05-20 15:24:56 +0300

    Add Coffee class with name validation, order tracking, and customer list methods

--- coffee.py
+++ coffee.py
@@ -5,6 +5,21 @@ class Coffee:
         if len(name) < 3:
             raise ValueError("Coffee name must be at least 3 characters")
         self._name = name
+        self._orders = []
 
     def get_name(self):
         return self._name
+
+    def add_order(self, order):
+        self._orders.append(order)
+
+    def get_orders(self):
+        return self._orders
+
+    def get_customers(self):
+        customers = []
+        for order in self._orders:
+            customer = order.get_customer()
+            if customer not in customers:
+                customers.append(customer)
+        return customers
